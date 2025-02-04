57322df8e9ae65937e90eae5a5ddd59707a914b7
Nikita Huliuk <nikaron4ik@gmail.com>
2025-02-04 13:46:49 +0300

    fix: Исправлена возможность класть яйца в раздатчик через быстрое перемещение из хот-бара

--- src/main/java/org/infernworld/inferncustomeggs/listener/Event.java
+++ src/main/java/org/infernworld/inferncustomeggs/listener/Event.java
@@ -7,8 +7,10 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockDispenseEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 import org.infernworld.inferncustomeggs.InfernCustomEggs;
 import org.infernworld.inferncustomeggs.item.Item;
 import org.infernworld.inferncustomeggs.util.Color;
@@ -50,6 +52,13 @@ public class Event implements Listener {
     public void onInventoryClick(InventoryClickEvent e) {
         if(e.getCurrentItem() != null && e.getInventory().getType() == InventoryType.DISPENSER) {
             if (e.getCurrentItem().isSimilar(items.items())) {
+                e.setCancelled(true);
+            }
+        }
+
+        if (e.getClick().isKeyboardClick()) {
+            ItemStack hotbarItem = e.getWhoClicked().getInventory().getItem(e.getHotbarButton());
+            if(hotbarItem != null && hotbarItem.isSimilar(items.items()) && e.getInventory().getType() == InventoryType.DISPENSER) {
                 e.setCancelled(true);
             }
         }
