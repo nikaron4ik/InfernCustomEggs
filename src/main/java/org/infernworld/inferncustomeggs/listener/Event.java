f14ef2218e0595b7bf0ed1332a15247612dc65cb
Nikita Huliuk <nikaron4ik@gmail.com>
2025-02-14 13:01:08 +0300

    fix: мелкие фиксы проблем, которые не влияли на игровой процесс но приводили к нежелательным ошибкам в консоли

--- src/main/java/org/infernworld/inferncustomeggs/listener/Event.java
+++ src/main/java/org/infernworld/inferncustomeggs/listener/Event.java
@@ -69,9 +69,12 @@ public class Event implements Listener {
         }
 
         if (e.getClick().isKeyboardClick()) {
-            ItemStack hotbarItem = e.getWhoClicked().getInventory().getItem(e.getHotbarButton());
-            if(hotbarItem != null && isCustomEgg(hotbarItem) && e.getInventory().getType() == InventoryType.DISPENSER) {
-                e.setCancelled(true);
+            int hotBarButton = e.getHotbarButton();
+            if (hotBarButton >= 0 && hotBarButton < e.getWhoClicked().getInventory().getSize()) {
+                ItemStack hotbarItem = e.getWhoClicked().getInventory().getItem(e.getHotbarButton());
+                if(hotbarItem != null && isCustomEgg(hotbarItem) && e.getInventory().getType() == InventoryType.DISPENSER) {
+                    e.setCancelled(true);
+                }
             }
         }
     }
